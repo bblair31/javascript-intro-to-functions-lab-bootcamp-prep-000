fa30bea354a4c7a66af95615e390e8a6660ef7a7
jbb341@hotmail.com <jbb341@hotmail.com>
2018-08-09 18:19:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,7 +15,8 @@ function logShout(string) {
 function logWhisper(string) {
   console.log('hello')
 }
-function sayHiToGrandma(string) {
- if (string === lowercase)
+function sayHiToGrandma(string)
+{ 
+  if (string === lowercase)
  return 'I can't hear you!'
  }
