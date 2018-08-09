c4e60ae80eb1d9aae91c15953f4fc6a8c92ad76c
jbb341@hotmail.com <jbb341@hotmail.com>
2018-08-09 18:34:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,7 +16,10 @@ function logWhisper(string) {
   console.log('hello')
 }
 function sayHiToGrandma(string){ 
- if (string === lowercase) {
+ {
+  if (string = "I love you, Grandma.")}
+ return 'I love you, too.'
+} else if (string === lowercase) {
    return "I can't hear you!"
 } else if (string === uppercase) {
    return "YES INDEED!"
