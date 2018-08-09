2e9537edeef3b9a1441a6785689a6b3992a68070
jbb341@hotmail.com <jbb341@hotmail.com>
2018-08-09 18:35:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function logShout(string) {
 function logWhisper(string) {
   console.log('hello')
 }
-function sayHiToGrandma(string){ 
+function sayHiToGrandma(string) { 
   if (string = "I love you, Grandma.") 
    
  return 'I love you, too.'
